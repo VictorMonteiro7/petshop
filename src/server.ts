f0a4db261b748131d7c9a689d77cc27b85186b1b
Victor Monteiro <contato@ovictormonteiro.com>
2022-03-03 19:37:13 -0300

    projeto finalizado

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ server.use(express.static(path.join(__dirname, "../public")));
 server.use(mainRoutes);
 
 server.use((req: Request, res: Response) => {
-  res.send("Página não encontrada");
+  res.render("pages/404");
 });
 
 // Rodando servidor
