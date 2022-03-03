bd5886adddbb1f33c9593c1cf52709b9446534fc
Victor Monteiro <contato@ovictormonteiro.com>
2022-03-03 17:35:45 -0300

    adicionado os controllers, rotas

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
+import mainRoutes from "./routes";
 
 // Setando Dotenv
 dotenv.config();
@@ -18,6 +19,11 @@ server.engine("mustache", mustache());
 server.use(express.static(path.join(__dirname, "../public")));
 
 // Rotas
+server.use(mainRoutes);
+
+server.use((req: Request, res: Response) => {
+  res.send("Página não encontrada");
+});
 
 // Rodando servidor
 server.listen(process.env.PORT);
